a205686dd3416de149b76410e257335672bb75a3
pabloferro <pablo.pyp@gmail.com>
2013-06-04 19:30:12 -0300

    Implemento categoría usando un Map

--- src/festival/model/Categoria.scala
+++ src/festival/model/Categoria.scala
@@ -1,5 +1,13 @@
 package festival.model
 
+object Categoria{
+  var instances: Map[Symbol, Categoria] = Map()
+  
+  def apply(s: Symbol) = instances.apply(s)
+  
+  def crearCategoria(key :Symbol, valor: Int) = instances+= key -> new Categoria(valor)
+}
+
 class Categoria(valor: Int) {
 
   def getValor = valor
--- src/festival/model/test/EntradaTest.scala
+++ src/festival/model/test/EntradaTest.scala
@@ -10,15 +10,20 @@ import org.junit.internal.runners.JUnit4ClassRunner
 
 @RunWith(classOf[JUnit4ClassRunner])
 class EntradaTest {
-  val ledZeppelin = new Banda(new Categoria(200))
-  val ironMaiden = new Banda(new Categoria(200))
-  val sodaStereo = new Banda(new Categoria(150))
+  Categoria.crearCategoria('categoria1, 0)
+  Categoria.crearCategoria('categoria2, 50)
+  Categoria.crearCategoria('categoria3, 100)
+  Categoria.crearCategoria('categoria4, 200)
+  
+  val ledZeppelin = new Banda(Categoria('categoria4))
+  val ironMaiden = new Banda(Categoria('categoria4))
+  val sodaStereo = new Banda(Categoria('categoria3))
   val noche1 = new Noche(Set(ledZeppelin, sodaStereo), new Fecha(2,10,2013))
   val festival = new Festival(Set(), new Fecha(1,6,2013), Set(noche1), Map('A'->Array(100,100,100), 'B'->Array(500,500,500)))
   
   @Test
   def testUnMenorCompraEntradaConValorBase100YBandasDeCategoria1Paga90 {
-    def noche = new Noche(Set(new Banda(new Categoria(0))), new Fecha(1,1,2014))
+    def noche = new Noche(Set(new Banda(Categoria('categoria1))), new Fecha(1,1,2014))
     def entrada = new Entrada(100, noche, new Menor(), 'A', 1);
     Assert.assertEquals(90.0, entrada.precio);
   }
