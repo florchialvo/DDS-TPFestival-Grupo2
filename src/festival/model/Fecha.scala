a68c0f8650c9f1219452946570d83f2988c3a606
Nico Braná <nicolasmbrana@gmail.com>
2013-06-05 10:36:55 -0300

    No usen el Ascii 164 o 165

--- src/festival/model/Fecha.scala
+++ src/festival/model/Fecha.scala
@@ -3,30 +3,30 @@ package festival.model
 import java.util.Calendar
 import java.text.SimpleDateFormat
 
-class Fecha(_dia: Int, _mes: Int, _año: Int) {
+class Fecha(_dia: Int, _mes: Int, _anio: Int) {
   
   def this() = this(0,0,0)
   
   def dia = _dia
   def mes = _mes
-  def año = _año
+  def anio = _anio
   
   def fechaActual():Fecha =  {
     val today = Calendar.getInstance().getTime()
     
     val diaFormat = new SimpleDateFormat("dd")
     val mesFormat = new SimpleDateFormat("MM")
-    val añoFormat = new SimpleDateFormat("yyyy")
+    val anioFormat = new SimpleDateFormat("yyyy")
     
     val diaObtenido = Integer.parseInt(diaFormat.format(today))
     val mesObtenido = Integer.parseInt(mesFormat.format(today))
-    val añoObtenido = Integer.parseInt(añoFormat.format(today))
+    val anioObtenido = Integer.parseInt(anioFormat.format(today))
     
-    return new Fecha(diaObtenido,mesObtenido,añoObtenido)    
+    return new Fecha(diaObtenido,mesObtenido,anioObtenido)    
   }
   
   
-  def toInt = this.año*10000+this.mes*100+this.dia
-  def ==(otraFecha: Fecha) = (this.año==otraFecha.año)&&(this.mes==otraFecha.mes)&&(this.dia==otraFecha.dia)
+  def toInt = this.anio*10000+this.mes*100+this.dia
+  def ==(otraFecha: Fecha) = (this.anio==otraFecha.anio)&&(this.mes==otraFecha.mes)&&(this.dia==otraFecha.dia)
   def >(otraFecha: Fecha) = this.toInt>otraFecha.toInt
 }
