1429ca8d74b9a1f0e721f2e8a5262a3a6b394fc6
Nico Braná <nicolasmbrana@gmail.com>
2013-06-26 01:50:34 -0300

    Refactor y dos test mas

--- TPGrupal Stock/reservaDeStockTest/TestManejoDeStock.scala
+++ TPGrupal Stock/reservaDeStockTest/TestManejoDeStock.scala
@@ -5,21 +5,42 @@ import reservaDeStock.ProductoCompuesto
 import reservaDeStock.MateriaPrima
 import org.junit.Test
 import reservaDeStock.Auditoria
+import reservaDeStock.ExcesoDeStock
+import reservaDeStock.NotificacionPorMail
+
+
 
 class TestManejoDeStock {
   
 	val fabrica:Fabrica = new Fabrica()
   
-	val volante:ProductoCompuesto = new ProductoCompuesto()
-	volante.stock_=(2)
+	val volante:MateriaPrima = new MateriaPrima()
+	val auto: ProductoCompuesto = new ProductoCompuesto()
 	  
 	@Test
 	def unaMateriaPrimaQuedaConStockMenorAlMinimoYGeneraPedidoDeCompra() {
-               volante.agregarInteresado(Auditoria)
-               volante.stockMin_=(2)
-               volante.consumir(1)
-               volante.saleComponente(1) 
+      	volante.stock_=(2)         
+      	volante.agregarInteresado(Auditoria)
+        volante.stockMin_=(2)
+        volante.consumir(1)
+        volante.saleComponente(1) 
 	}
 	
+	@Test
+	def unaMateriaPrimaTieneExcesoDeStock() {
+		val excesoDeStock: ExcesoDeStock = new ExcesoDeStock(true)
+		volante.agregarInteresado(excesoDeStock)
+		volante.stockMax_=(5)
+		volante.stock_=(4)
+		volante.entraComponente(3)
+	}
 	
+	@Test
+	def unProductoFinalEsVendidoMasDe3VecesGenerandoUnMail(){
+		val notificacion: NotificacionPorMail = new NotificacionPorMail(3)
+		volante.agregarInteresado(notificacion)
+		volante.stock_(5)
+		volante.consumir(4)
+		volante.saleComponente(4)
+	}
 }
--- test/festival/model/EntradaTest.scala
+++ test/festival/model/EntradaTest.scala
@@ -7,7 +7,6 @@ import junit.framework.Assert
 import org.junit._
 import org.junit.runner.RunWith
 import org.junit.internal.runners.JUnit4ClassRunner
-import festival.model.FestivalMock
 import festival.model.exception.BusinessException
 import org.junit.Test
 import org.junit.internal.runners.JUnit4ClassRunner
