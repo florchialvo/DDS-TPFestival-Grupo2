a436b789408ac1df5073c3fe317f1882478c5376
kevin <kevin@kevin-AO532h>
2013-06-25 22:35:10 -0300

    TestReserva.scala

---
+++ TPGrupal Stock/reservaDeStock/TestReserva.scala
@@ -0,0 +1,19 @@
+package reservaDeStock
+
+
+import festival.model._
+import junit.framework.Assert
+import org.junit._
+import org.junit.runner.RunWith
+import org.junit.internal.runners.JUnit4ClassRunner
+import festival.model.FestivalMock
+import org.junit.Test
+import org.junit.internal.runners.JUnit4ClassRunner
+import org.junit.runner.RunWith
+
+@RunWith(classOf[JUnit4ClassRunner])
+class TestReserva {
+  
+
+
+}
