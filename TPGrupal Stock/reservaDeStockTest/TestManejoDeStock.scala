a13f7331107ea2673941720ba924a3b6da19aefd
Nico Braná <nicolasmbrana@gmail.com>
2013-06-26 04:07:40 -0300

    El test como debe ser

--- TPGrupal Stock/reservaDeStockTest/TestManejoDeStock.scala
+++ TPGrupal Stock/reservaDeStockTest/TestManejoDeStock.scala
@@ -7,6 +7,7 @@ import org.junit.Test
 import reservaDeStock.Auditoria
 import reservaDeStock.ExcesoDeStock
 import reservaDeStock.NotificacionPorMail
+import org.junit.Assert
 
 
 
@@ -24,6 +25,7 @@ class TestManejoDeStock {
         volante.stockMin_=(2)
         volante.consumir(1)
         volante.saleComponente(1) 
+        Assert.assertTrue(Auditoria.cumpleRequisitoSalida(volante, 1))
 	}
 	
 	@Test
@@ -33,14 +35,16 @@ class TestManejoDeStock {
 		volante.stockMax_=(5)
 		volante.stock_=(4)
 		volante.entraComponente(3)
+		Assert.assertTrue(excesoDeStock.cumpleRequisitoEntrada(volante, 3))
 	}
 	
 	@Test
 	def unProductoFinalEsVendidoMasDe3VecesGenerandoUnMail(){
 		val notificacion: NotificacionPorMail = new NotificacionPorMail(3)
-		volante.agregarInteresado(notificacion)
-		volante.stock_(5)
-		volante.consumir(4)
-		volante.saleComponente(4)
+		auto.agregarInteresado(notificacion)
+		auto.stock_=(5)
+		auto.consumir(4)
+		auto.saleComponente(4)
+		Assert.assertTrue(notificacion.cumpleRequisitoSalida(auto, 4))
 	}
 }
