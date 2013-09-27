458b6ce92c459ab47ecef1b14d035429e08823a2
pabloferro <pablo.pyp@gmail.com>
2013-09-27 17:03:45 -0300

    En anular entrada cargo la lista de tipos de persona (como ejemplo)

--- Festival_ui/src/main/scala/ddsGrupo2/festival/ui/AnularPage.scala
+++ Festival_ui/src/main/scala/ddsGrupo2/festival/ui/AnularPage.scala
@@ -6,30 +6,34 @@ import org.apache.wicket.markup.html._
 import org.apache.wicket.markup.html.form._
 import org.apache.wicket.model._
 import org.apache.wicket.markup.html.basic.Label
+import collection.JavaConversions._
 
 import ddsGrupo2.festival.model._
 
 class AnularPage extends WebPage {
-	 val form = new Form("form")
-	 form.add(new Label("label", 
-	     "Seleccione Fecha, Sector y Fila de la entrada que desea anular"))
-	 addLabels(form)
-	 addOptions(form)
-	 addActions(form)
-	 add(form) 
-	 
-	 def addLabels(form: Form){
-	   form.add(new Label("labelFecha", "Fecha"))
-	   form.add(new Label("labelSector", "Sector"))
-	   form.add(new Label("labelFila", "Fila"))
-	 }
-	 
-	 def addOptions(form: Form){
-	    
-	 }
-	 
-	 def addActions(form: Form){
-	   
-	 }
+    val form = new Form("entradaForm", this.createModel)
+    val festival = FestivalesHome.getFestival
 
+    addFields(form)
+    addOptions(form)
+    addActions(form)
+    add(form)
+
+    def addFields(form: Form) {
+        //        form.add(new Label("labelFecha", "Fecha"))
+        //        form.add(new Label("labelSector", "Sector"))
+        //        form.add(new Label("labelFila", "Fila"))
+        form.add(new DropDownChoice("tipoPersona", this.descuentosValidos))
+    }
+
+    def addOptions(form: Form) {
+    }
+
+    def addActions(form: Form) {
+    }
+
+    def createModel: CompoundPropertyModel = new CompoundPropertyModel(this.festival);
+
+    def descuentosValidos: java.util.List[TipoPersona] =
+        new java.util.ArrayList[TipoPersona](this.festival.descuentosValidos)
 }
