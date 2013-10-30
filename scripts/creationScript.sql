76b63f2815f953c2abbba967253f8d0baeb93aa5
kevin1992 <kevin1992@gmail.com>
2013-10-30 11:40:22 -0300

    Agregadas las querys

--- scripts/creationScript.sql
+++ scripts/creationScript.sql
@@ -1,6 +1,19 @@
 USE TpFestival
 	GO
 
+
+--DROPS
+DROP TABLE Entradas
+DROP TABLE TiposPersona
+DROP TABLE FilaPorSector
+DROP TABLE Sectores
+DROP TABLE BandaPorNoche
+DROP TABLE Bandas
+DROP TABLE Categorias
+DROP TABLE Noches
+DROP TABLE Festivales
+
+
 CREATE TABLE [Festivales] (
 	festival_Id NUMERIC(18,0) PRIMARY KEY IDENTITY,
 	festival_Nombre NVARCHAR(255),
@@ -25,9 +38,12 @@ CREATE TABLE [Bandas](
 )
 
 CREATE TABLE [BandaPorNoche](
-	bandaPorNoche_Id NUMERIC(18,0) PRIMARY KEY IDENTITY,
+
 	bandaPorNoche_CodigoBanda NUMERIC(18,0) FOREIGN KEY REFERENCES Bandas (banda_Id),
 	bandaPorNoche_CodigoNoche NUMERIC(18,0) FOREIGN KEY REFERENCES Noches (noche_Id), 
+	bandaPorNoche_HorarioComienzo DATETIME,
+	bandaPorNoche_HorarioFin DATETIME,
+	CONSTRAINT PK_BANDAPORNOCHE PRIMARY KEY (bandaPorNoche_CodigoBanda,bandaPorNoche_CodigoNoche)
 )
 
 CREATE TABLE [Sectores](
@@ -98,14 +114,14 @@ INSERT INTO Noches VALUES ('21/10/2014',1)
 INSERT INTO Noches VALUES ('18/10/2014',2)
 INSERT INTO Noches VALUES ('19/10/2014',2)
 
---BANDAS X NOCHE (Id_banda,Id_noche)
-INSERT INTO BandaPorNoche VALUES (1,1)
-INSERT INTO BandaPorNoche VALUES (3,1)
-INSERT INTO BandaPorNoche VALUES (5,2)
-INSERT INTO BandaPorNoche VALUES (1,3)
-INSERT INTO BandaPorNoche VALUES (2,3)
-INSERT INTO BandaPorNoche VALUES (1,4)
-INSERT INTO BandaPorNoche VALUES (3,4)
+--BANDAS X NOCHE (Id_banda,Id_noche,fechaComienzo,fechaFin)
+INSERT INTO BandaPorNoche VALUES (1,1,'12:00:00','14:00:00')
+INSERT INTO BandaPorNoche VALUES (3,1,'14:00:00','16:00:00')
+INSERT INTO BandaPorNoche VALUES (5,2,'12:00:00','14:00:00')
+INSERT INTO BandaPorNoche VALUES (1,3,'14:00:00','16:00:00')
+INSERT INTO BandaPorNoche VALUES (2,3,'16:00:00','18:00:00')
+INSERT INTO BandaPorNoche VALUES (1,4,'12:00:00','14:00:00')
+INSERT INTO BandaPorNoche VALUES (3,4,'14:00:00','17:00:00')
 
 
 
@@ -157,20 +173,45 @@ INSERT INTO Entradas VALUES(1,2,3,2,2,'Flor',2,1,'V')
 INSERT INTO Entradas VALUES(1,2,2,1,2,'Pablo',2,1,'V')
 INSERT INTO Entradas VALUES(1,3,3,2,3,'Nico',1,1,'V')
 INSERT INTO Entradas VALUES(1,3,2,1,4,'Pepe',3,1,'V')
+INSERT INTO Entradas VALUES(2,3,2,1,4,'Tomas',3,1,'V')
+INSERT INTO Entradas VALUES(2,3,2,1,4,'Yago',3,1,'V')
 
 
---DROPS
-DROP TABLE Entradas
-DROP TABLE TiposPersona
-DROP TABLE FilaPorSector
-DROP TABLE Sectores
-DROP TABLE BandaPorNoche
-DROP TABLE Bandas
-DROP TABLE Categorias
-DROP TABLE Noches
-DROP TABLE Festivales
 
 
 --Integrante 1: información sobre las entradas vendidas a un usuario/cliente (ej: “Kevin”)
+SELECT festival_Nombre,CONVERT(VARCHAR(10),noche_Fecha,101)'Fecha Noche',sector_Letra,entrada_Fila,entrada_Butaca,entrada_Cliente,tipoPersona_Nombre,entrada_PuntoDeVenta 
+		FROM ENTRADAS,FESTIVALES,TiposPersona,Sectores,Noches
+		WHERE 
+			festival_Id = entrada_Festival and
+			noche_Id = entrada_Noche and
+			sector_Id = entrada_Sector and
+			tipoPersona_Id = entrada_TipoPersona and
+			entrada_cliente='Kevin'
+			
+--Integrante 2: información sobre las entradas vendidas para un festival (ej: “Jesus María Rock”)
+SELECT festival_Nombre,CONVERT(VARCHAR(10),noche_Fecha,101)'Fecha Noche',sector_Letra,entrada_Fila,entrada_Butaca,entrada_Cliente,tipoPersona_Nombre,entrada_PuntoDeVenta 
+	FROM ENTRADAS,FESTIVALES,TiposPersona,Sectores,Noches	
+	WHERE 
+	festival_Id = entrada_Festival and
+	noche_Id = entrada_Noche and		
+	sector_Id = entrada_Sector and
+	tipoPersona_Id = entrada_TipoPersona and
+	festival_Nombre= 'Festival Rock' and entrada_Festival= festival_Id
 
-SELECT * FROM ENTRADAS WHERE entrada_cliente='Kevin'
+--Integrante 3: información sobre las presentaciones de una banda en distintos festivales (ej: “Catupecu Machu”)
+SELECT banda_Nombre,festival_Nombre,CONVERT(VARCHAR(10),noche_Fecha,101)'Fecha Noche' 
+	FROM Bandas,BandaPorNoche,Noches, Festivales 
+	WHERE 
+			banda_Id = bandaPorNoche_CodigoBanda and banda_Nombre = 'Led Zeppelin' and
+			noche_Id = bandaPorNoche_CodigoNoche and noche_Festival = festival_Id
+
+
+--Integrante 4: información sobre todas las bandas que se presentan una noche, con el horario de comienzo y fin.
+SELECT banda_Nombre,CONVERT(VARCHAR(10),noche_Fecha,101)'Fecha Noche' ,CONVERT(VARCHAR(10),bandaPorNoche_HorarioComienzo,108)'Fecha Comienzo',CONVERT(VARCHAR(10),bandaPorNoche_HorarioFin,108)'Fecha Comienzo'
+	FROM Bandas,BandaPorNoche,Noches
+	WHERE
+			banda_Id = bandaPorNoche_CodigoBanda and
+			noche_Id = bandaPorNoche_CodigoNoche
+			
+	
